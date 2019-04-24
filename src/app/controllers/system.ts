d6ee124d3794d7eb4eace1aa9d6d667648b016d0
Erin Corson <frodopwns@gmail.com>
2019-04-24 11:34:25 -0600

    real health check (#71)

--- src/app/controllers/system.ts
+++ src/app/controllers/system.ts
@@ -1,4 +1,6 @@
+import { DocumentQuery } from "documentdb";
 import { ServiceLocator } from "../../config/servicelocator";
+import { collection, database } from "../../db/dbconstants";
 
 /**
  * Health check controller
@@ -8,9 +10,21 @@ export async function healthcheck(req, res) {
     // Stub:
     const locator = await ServiceLocator.getInstance();
     // @todo query cosmos and return 200 if it works
-    // const cosmosDb = locator.getCosmosDB();
+    const cosmosDb = locator.getCosmosDB();
     const telem = locator.getTelemClient();
 
     telem.trackEvent("healthcheck called");
+
+    const querySpec: DocumentQuery  = {
+        parameters: [],
+        query: "SELECT * FROM root",
+    };
+
+    try {
+        const results = await cosmosDb.queryCollections(database, querySpec);
+    } catch (e) {
+        return res.send(500, {message: "Application failed to reach database"});
+    }
+
     return res.send(200, {message: "Successfully reached healthcheck endpoint"});
 }
