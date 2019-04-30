1983bf176c91c559e67ce28ea317466de918b691
Mel Rush <melanierush@live.com>
2019-04-30 15:38:54 -0600

    fixed actorsRoute to correct endpoint (#102)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as ApplicationInsights from "applicationinsights";
 import * as bodyParser from "body-parser";
 import * as restify from "restify";
-import * as actorRoutes from "./app/routes/genre";
+import * as actorRoutes from "./app/routes/actor";
 import * as genreRoutes from "./app/routes/genre";
 import * as movieRoutes from "./app/routes/movie";
 import * as systemRoutes from "./app/routes/system";
