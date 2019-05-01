a0066be2699ed911c65dc9e6e6c249d842e3ef41
Amanda Kaufman <aka@microsoft.com>
2019-05-01 11:30:42 -0600

    Added /api/actors integration test

--- src/app/tests/actor.integration.ts
+++ src/app/tests/actor.integration.ts
@@ -8,8 +8,19 @@ chai.use(chaiHttp);
 
 describe("Testing Actor Controller Methods", () => {
 
+  it("Testing /api/actors", async () => {
+    const queryId = "nm0000323";
+    return chai.request(server)
+    .get(`/api/actors`)
+    .then((res) => {
+      chai.expect(res).to.have.status(200);
+      const id = res.body[0].actorId;
+      chai.assert.equal(queryId, id);
+    });
+  });
+
   it("Testing /api/actors/:id", async () => {
-    const queryId = "nm0000345";
+    const queryId = "nm0000323";
     return chai.request(server)
     .get(`/api/actors/${queryId}`)
     .then((res) => {
