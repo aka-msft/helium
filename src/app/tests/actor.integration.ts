4ac799ead7f3ff86c4ae75fc8599bbf1321119ab
Amanda Kaufman <47929154+aka-msft@users.noreply.github.com>
2019-05-01 13:05:01 -0600

    Added integration test for /api/actors endpoint. (#109)
    * Added /api/actors integration test
    
    * Updated server url in actors test

--- src/app/tests/actor.integration.ts
+++ src/app/tests/actor.integration.ts
@@ -2,14 +2,25 @@ import * as chai from "chai";
 import chaiHttp = require("chai-http");
 import "mocha";
 
-const server = "https://seusherhelium.azurewebsites.net/";
+const server = "https://heliumint.azurewebsites.net/";
 
 chai.use(chaiHttp);
 
 describe("Testing Actor Controller Methods", () => {
 
+  it("Testing /api/actors", async () => {
+    const queryId = "nm0000323";
+    return chai.request(server)
+    .get(`/api/actors`)
+    .then((res) => {
+      chai.expect(res).to.have.status(200);
+      const id = res.body[0].actorId;
+      chai.assert.equal(queryId, id);
+    });
+  });
+
   it("Testing /api/actors/:id", async () => {
-    const queryId = "nm0000345";
+    const queryId = "nm0000323";
     return chai.request(server)
     .get(`/api/actors/${queryId}`)
     .then((res) => {
