ea6c7c8d68598b6bcaaecba666c54d205312f294
Amanda Kaufman <aka@microsoft.com>
2019-05-30 14:29:59 -0600

    Fixed source URLs for references to modules in Cobalt

--- templates/helium-simple.tf
+++ templates/helium-simple.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "helium_resource" {
 }
 
 module "service_plan" {
-  source                  = "github.com/microsoft/cobalt/tree/master/infra/modules/providers/azure/service-plan"
+  source                  = "github.com/microsoft/cobalt/infra/modules/providers/azure/service-plan"
   resource_group_name     = "${azurerm_resource_group.helium_resource.name}"
   resource_group_location = "${azurerm_resource_group.helium_resource.location}"
   service_plan_name       = "${var.service_plan_name}"
 }
 
 module "app_insights" {
-  source                               = "github.com/microsoft/cobalt/tree/master/infra/modules/providers/azure/app-insights"
+  source                               = "github.com/microsoft/cobalt/infra/modules/providers/azure/app-insights"
   service_plan_resource_group_name     = "${azurerm_resource_group.helium_resource.name}"
   appinsights_name                     = "${var.appinsights_name}"
   application_type                     = "${var.appinsights_application_type}"
@@ -19,7 +19,7 @@ module "app_insights" {
 }
 
 module "cosmosdb" {
-    source                             = "github.com/microsoft/cobalt/tree/master/infra/modules/providers/azure/cosmosdb"
+    source                             = "github.com/microsoft/cobalt/infra/modules/providers/azure/cosmosdb"
     service_plan_resource_group_name   = "${azurerm_resource_group.helium_resource.name}"
     cosmosdb_name                      = "${var.cosmosdb_name}"
     cosmosdb_kind                      = "${var.cosmosdb_kind}"
