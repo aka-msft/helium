0002b947d02a23f161206567807fb39e3201f21f
Erin Corson <frodopwns@gmail.com>
2019-07-16 10:31:26 -0600

    refactor tests to fix orphaned movies (#318)
    * tests were not cleaning up properly, hopefully this helps
    
    * refactoring
    
    * removing async keywork from race-condition prone sections
    
    * can't use arrow funcs for mocha features to work

--- src/app/tests/movie.integration.ts
+++ src/app/tests/movie.integration.ts
@@ -6,172 +6,259 @@ import { StringUtilities } from "../../utilities/stringUtilities";
 
 chai.use(chaiHttp);
 
-describe("Testing Movie Controller Methods", () => {
+describe("Testing Movie Controller Methods", function() {
 
-  it("Testing GET /api/movies", async () => {
-    return chai.request(integrationServer)
-      .get(`/api/movies`)
-      .then((res) => {
-        chai.expect(res).to.have.status(200);
-        const body = res.body;
-        chai.assert.isArray(body);
-      });
+  describe("GET /api/movies", () => {
+    it("Should return a list ", async () => {
+      chai.request(integrationServer)
+        .get(`/api/movies`)
+        .then((res) => {
+          chai.expect(res).to.have.status(200);
+          const body = res.body;
+          chai.assert.isArray(body);
+        });
+    });
   });
 
-  it("Testing POST + GET /api/movies/:id", async () => {
-    const randomString = StringUtilities.getRandomString();
+  describe("Test the ability to create and get movies", function() {
+      const randomString = "z" + StringUtilities.getRandomString();
 
-    const testMovie = {
-      genres: [],
-      id: randomString,
-      key: "0",
-      movieId: randomString,
-      roles: [],
-      runtime: 120,
-      textSearch: randomString.toLowerCase(),
-      title: randomString,
-      type: "Movie",
-      year: 1994,
-    };
+      const testMovie = {
+        genres: [],
+        id: randomString,
+        key: "0",
+        movieId: randomString,
+        roles: [],
+        runtime: 120,
+        textSearch: randomString.toLowerCase(),
+        title: randomString,
+        type: "Movie",
+        year: 1994,
+      };
 
-    return chai.request(integrationServer)
-      .post("/api/movies")
-      .set("content-type", "application/json")
-      .send(testMovie)
-      .then((res) => {
-
-        chai.expect(res).to.have.status(201);
-        chai.request(integrationServer)
-          .get(`/api/movies/${randomString}`)
-          .then((getResponse) => {
-            chai.expect(getResponse).to.have.status(200);
-            const getRespBody = getResponse.body;
-            chai.assert.isNotArray(getRespBody);
-            chai.assert.equal(randomString, getRespBody.movieId);
-            chai.assert.equal(randomString, getRespBody.title);
-          });
-      });
-
-  });
-
-  it("Testing POST + GET /api/movies?q=<name>", async () => {
-    const randomString = StringUtilities.getRandomString();
-
-    const testMovie = {
-      genres: [],
-      id: randomString,
-      key: "0",
-      movieId: randomString,
-      roles: [],
-      runtime: 120,
-      textSearch: randomString.toLowerCase(),
-      title: randomString,
-      type: "Movie",
-      year: 1994,
-    };
-
-    return chai.request(integrationServer)
-      .post("/api/movies")
-      .set("content-type", "application/json")
-      .send(testMovie)
-      .then((res) => {
-        chai.expect(res).to.have.status(201);
-        chai.request(integrationServer)
-          .get(`/api/movies`)
-          .query({ q: randomString })
-          .then((getResponse) => {
-            chai.expect(getResponse).to.have.status(200);
-            const getRespBody = getResponse.body;
-            chai.assert.isArray(getRespBody);
-            chai.assert.isAtLeast(getRespBody.length, 1);
-            chai.assert.equal(randomString, getRespBody[0].movieId);
-            chai.assert.equal(randomString, getRespBody[0].title);
-          });
-      });
-  });
-
-  it("Testing POST + DELETE + GET /api/movies", async () => {
-    const randomString = StringUtilities.getRandomString();
-
-    const testMovie = {
-      genres: [],
-      id: randomString,
-      key: "0",
-      movieId: randomString,
-      roles: [],
-      runtime: 120,
-      textSearch: randomString.toLowerCase(),
-      title: randomString,
-      type: "Movie",
-      year: 1994,
-    };
-
-    return chai.request(integrationServer)
-      .post("/api/movies")
-      .set("content-type", "application/json")
-      .send(testMovie)
-      .then((res) => {
-        chai.expect(res).to.have.status(201);
-        const delReq = chai.request(integrationServer);
-        delReq.del(`/api/movies/${randomString}`).set("content-type", "application/json").then((getResponse) => {
-          chai.expect(getResponse).to.have.status(204);
+      describe("POST /api/movies", () => {
+        it("Should create a movie", () => {
           chai.request(integrationServer)
-            .get(`/api/movies/${randomString}`)
-            .then((getRes) => {
-              chai.expect(getRes).to.have.status(404);
-            });
-
+          .post("/api/movies")
+          .set("content-type", "application/json")
+          .send(testMovie)
+          .end((err, res) => {
+            chai.expect(res).to.have.status(201);
+          });
         });
       });
+
+      describe("GET /api/movies/" + testMovie.id, function() {
+        it("Should return the movie", function() {
+          chai.request(integrationServer)
+            .get(`/api/movies/${randomString}`)
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(200);
+              const getRespBody = getResponse.body;
+              chai.assert.isNotArray(getRespBody);
+              chai.assert.equal(randomString, getRespBody.movieId);
+              chai.assert.equal(randomString, getRespBody.title);
+            });
+        });
+      });
+
+      describe("DELETE /api/movies/" + testMovie.id, () => {
+        it("Should remove the movie", async () => {
+          chai.request(integrationServer)
+            .del(`/api/movies/${randomString}`)
+            .set("content-type", "application/json")
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(204);
+            });
+        });
+      });
+
   });
 
-  it("Testing POST + PUT + GET /api/movies", async () => {
-    const randomString = StringUtilities.getRandomString();
+  describe("Test the abiilty to filter movies by title", () => {
+      const randomString = "z" + StringUtilities.getRandomString();
 
-    const testMovie = {
-      genres: [],
-      id: randomString,
-      key: "0",
-      movieId: randomString,
-      roles: [],
-      runtime: 120,
-      textSearch: randomString.toLowerCase(),
-      title: randomString,
-      type: "Movie",
-      year: 1994,
-    };
+      const testMovie = {
+        genres: [],
+        id: randomString,
+        key: "0",
+        movieId: randomString,
+        roles: [],
+        runtime: 120,
+        textSearch: randomString.toLowerCase(),
+        title: randomString,
+        type: "Movie",
+        year: 1994,
+      };
 
-    return chai.request(integrationServer)
-      .post("/api/movies")
-      .set("content-type", "application/json")
-      .send(testMovie)
-      .then((res) => {
-        chai.expect(res).to.have.status(201);
-        testMovie.title = randomString + "1";
-        testMovie.textSearch = randomString + "1";
-        const putReq = chai.request(integrationServer);
-        putReq.put(`/api/movies/${randomString}`).set("content-type", "application/json").send(testMovie)
-          .then((getResponse) => {
-            chai.expect(getResponse).to.have.status(201);
-
-            chai.request(integrationServer)
-              .get(`/api/movies/${randomString}`)
-              .then((getRes) => {
-                chai.expect(getRes).to.have.status(200);
-                chai.expect(getRes.body.title).to.equal(`${randomString}1`);
-              });
-
+      describe("POST /api/movies", () => {
+        it("Should create a movie", async () => {
+          chai.request(integrationServer)
+          .post("/api/movies")
+          .set("content-type", "application/json")
+          .send(testMovie)
+          .end((err, res) => {
+            chai.expect(res).to.have.status(201);
           });
+        });
       });
+
+      describe("GET /api/movies?q=" + randomString, () => {
+        it("Should return the movie", async () => {
+          chai.request(integrationServer)
+            .get(`/api/movies`)
+            .query({ q: randomString })
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(200);
+              const getRespBody = getResponse.body;
+              chai.assert.isArray(getRespBody);
+              chai.assert.isAtLeast(getRespBody.length, 1);
+              chai.assert.equal(randomString, getRespBody[0].movieId);
+              chai.assert.equal(randomString, getRespBody[0].title);
+            });
+        });
+      });
+
+      describe("DELETE /api/movies/" + testMovie.id, () => {
+        it("Should remove the movie", async () => {
+          chai.request(integrationServer)
+            .del(`/api/movies/${randomString}`)
+            .set("content-type", "application/json")
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(204);
+            });
+        });
+      });
+
   });
 
-  it("Testing POST Bad Request Response Code", async () => {
-    return chai.request(integrationServer)
-      .post("/api/movies")
-      .set("content-type", "application/json")
-      .send({})
-      .catch((err) => {
-        chai.expect(err.response).to.have.status(400);
+  describe("Test ability to delete a movie", () => {
+      const randomString = "z" + StringUtilities.getRandomString();
+
+      const testMovie = {
+        genres: [],
+        id: randomString,
+        key: "0",
+        movieId: randomString,
+        roles: [],
+        runtime: 120,
+        textSearch: randomString.toLowerCase(),
+        title: randomString,
+        type: "Movie",
+        year: 1994,
+      };
+
+      describe("POST /api/movies", () => {
+        it("Should create a movie", async () => {
+          chai.request(integrationServer)
+          .post("/api/movies")
+          .set("content-type", "application/json")
+          .send(testMovie)
+          .end((err, res) => {
+            chai.expect(res).to.have.status(201);
+          });
+        });
       });
+
+      describe("DELETE /api/movies/" + testMovie.id, () => {
+        it("Should remove the movie", async () => {
+          chai.request(integrationServer)
+            .del(`/api/movies/${randomString}`)
+            .set("content-type", "application/json")
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(204);
+            });
+        });
+      });
+
+      describe("GET /api/movies/" + randomString, () => {
+        it("Should not return the movie", async () => {
+          chai.request(integrationServer)
+            .get(`/api/movies`)
+            .query({ q: randomString })
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(404);
+            });
+        });
+      });
+
+  });
+
+  describe("POST + PUT + GET + DELETE /api/movies", () => {
+      const randomString = "zzz" + StringUtilities.getRandomString();
+
+      const testMovie = {
+        genres: [],
+        id: randomString,
+        key: "0",
+        movieId: randomString,
+        roles: [],
+        runtime: 120,
+        textSearch: randomString.toLowerCase(),
+        title: randomString,
+        type: "Movie",
+        year: 1994,
+      };
+
+      describe("POST /api/movies", () => {
+        it("Should create a movie", async () => {
+          chai.request(integrationServer)
+          .post("/api/movies")
+          .set("content-type", "application/json")
+          .send(testMovie)
+          .end((err, res) => {
+            chai.expect(res).to.have.status(201);
+          });
+        });
+      });
+
+      describe("PUT /api/movies/" + testMovie.id, () => {
+        it("Should update a movie", async () => {
+          testMovie.title = randomString + "1";
+          testMovie.textSearch = randomString + "1";
+          chai.request(integrationServer)
+          .put(`/api/movies/${randomString}`)
+          .set("content-type", "application/json")
+          .send(testMovie)
+          .end((err, res) => {
+            chai.expect(res).to.have.status(201);
+          });
+        });
+      });
+
+      describe("GET /api/movies/" + testMovie.id, () => {
+        it("Should return the movie", async () => {
+          chai.request(integrationServer)
+            .get(`/api/movies/${randomString}`)
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(200);
+              chai.expect(getResponse.body.title).to.equal(`${randomString}1`);
+            });
+        });
+      });
+
+      describe("DELETE /api/movies/" + testMovie.id, () => {
+        it("Should remove the movie", async () => {
+          chai.request(integrationServer)
+            .del(`/api/movies/${randomString}`)
+            .set("content-type", "application/json")
+            .end((err, getResponse) => {
+              chai.expect(getResponse).to.have.status(204);
+            });
+        });
+      });
+
+  });
+
+  describe("POST /api/movies", () => {
+    it("should test POST Bad Request Response Code", async () => {
+      chai.request(integrationServer)
+        .post("/api/movies")
+        .set("content-type", "application/json")
+        .send({})
+        .catch((err) => {
+          chai.expect(err.response).to.have.status(400);
+        });
+    });
   });
 });
